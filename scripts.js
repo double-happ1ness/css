fda86771918281ba10d4c8c21db71694d0cbabf3
double-happ1ness <42737000+double-happ1ness@users.noreply.github.com>
2019-01-07 15:51:16 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,34 +1,59 @@
-<script src="http://code.jquery.com/jquery-latest.js"></script>
-<script language="javascript">
-$(function() {
-// set a event handler to the button
-  $("#btn_login").click(function() {
-    // retrieve form data
-    var uname = $("#uname").val();
-    var psw = $("#psw").val();
+<script>
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
-    // send form data to the server side php script.
-    $.ajax({
-        type: "POST",
-        url: "checkPassword.php",
-        data: { uname:uname, psw:psw }
-    }).done(function( data ) {
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
 
-        // Now the output from PHP is set to 'data'.
-        // Check if the 'data' contains 'OK' or 'NG'
-        if (data.indexOf("OK") >= 0){
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
 
-            // you can do something here
-            alert("Login Successfull.");
-            location.href = "ok.html";
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) { 
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
 
-        }else if(data.indexOf("NG") >= 0){
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) { 
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
 
-            // you can do something here
-            alert("Login Failed.");
-            location.href = "ng.html";
-        }
-    });
-  });
-});
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) { 
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
 </script>
