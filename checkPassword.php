70b9ca50458a34ac5b69f2aa3de7367951223730
double-happ1ness <42737000+double-happ1ness@users.noreply.github.com>
2019-01-07 15:24:42 +0000

    Create checkPassword.php

---
+++ checkPassword.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+      1.Retrieve post data : "uname" and "pass".
+      2.Check if  user name and password is valid.
+    */
+    if( /* valid user name and password */ ){
+        echo "OK";
+    }else{
+        echo "NG";
+    }
+?>
