5b03190bacd66ca90fb82dc0d1539f79c0f71140
double-happ1ness <42737000+double-happ1ness@users.noreply.github.com>
2019-01-28 14:49:34 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -57,9 +57,19 @@ myInput.onkeyup = function() {
   }
 }
 
-if (condition) {
-  //  redirect to https://double-happ1ness.github.io/css/success.html
-} else { 
-  //  redirect to https://double-happ1ness.github.io/css/fail.html
-}
+<script type="text/javascript">
+    function validateForm() {
+        var un = document.loginForm.username.value;
+        var pw = document.loginForm.password.value;
+        var username = "MitchWardle"; 
+        var password = "123abc456";
+        if ((un == username) && (pw == password)) {
+            window.location = "content.html";
+            return false;
+        }
+        else {
+            alert ("Login was unsuccessful, please check your username and password");
+        }
+    }
+</script>
 </script>
